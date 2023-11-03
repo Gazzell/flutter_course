6e54d56b0986d0ab58274a2bbb5df1af7d53bc37
Joseba Rodriguez <static.jrc@gmail.com>
2023-11-03 15:19:38 +0100

    chore: reflects imperative url in gorouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:todo_app/0_data/data_sources/local/hive_local_data_source.dart';
 import 'package:todo_app/0_data/repositories/todo_repository.local.dart';
 import 'package:todo_app/1_domain/repositories/todo_repository.dart';
@@ -7,7 +8,10 @@ import 'package:todo_app/2_application/app/basic_app.dart';
 
 Future<void> main() async {
   final localDataSource = HiveLocalDataSource();
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+
   await localDataSource.init();
+  
   runApp(
     RepositoryProvider<ToDoRepository>(
       create: (context) => ToDoRepositoryLocal(
